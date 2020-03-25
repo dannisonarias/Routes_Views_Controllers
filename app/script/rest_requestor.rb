da704df0558b51940298eb4385280e84dd354fc2
DannisonArias <dannisonarias@gmail.com>
2020-03-25 18:20:02 -0400

    create method for each GET request

--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -1,5 +1,8 @@
 require 'rest-client'
-url = "http://localhost:3000/users"
+show_url = "http://localhost:3000/users/show"
+new_url = "http://localhost:3000/users/new"
+index_url = "http://localhost:3000/users"
+edit_url = "http://localhost:3000/users/1/edit"
 
 
-puts RestClient.get(url)
+puts RestClient.get(edit_url)
